e9e1eedfc82de65a985b0056655e2d133f687142
John-Patrick-Narvasa <narvasajohnpatrick@gmail.com>
2024-09-18 02:11:52 +0900

    practice strat established

---
+++ _SchoolStuff/Study/Arithmetics2.java
@@ -0,0 +1,9 @@
+package _SchoolStuff.Study;
+
+public class Arithmetics2 {
+    public static void main(String[] args) {
+        int x = 11;
+        int result = x++ * 3 + --x;
+        System.out.println(result);
+    }
+}
